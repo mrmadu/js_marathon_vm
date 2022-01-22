1d9830d97a4912c94428d6380bd06b6697249fc2
madu <mr.madu@ya.ru>
2022-01-22 11:32:22 +0300

    ready

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+const slides = document.querySelectorAll('.slide');
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearAllActiveClasses();
+        slide.classList.add('active');
+    });
+}
+
+function clearAllActiveClasses() {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+}
